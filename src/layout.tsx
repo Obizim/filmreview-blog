0c00397268806825f7ad30d513fb734c3434522f
Zim <obizimuzo@gmail.com>
2023-05-16 16:52:06 +0100

    update Readme

--- src/layout.tsx
+++ src/layout.tsx
@@ -2,13 +2,13 @@ import { Fira_Sans } from 'next/font/google'
 import { Nav } from "./components/nav"
 import { Footer } from './components/footer'
 
-const marcellus = Fira_Sans({ subsets: ['latin'], weight: "400" })
+const firaSans = Fira_Sans({ subsets: ['latin'], weight: "400" })
 type bodyProps = {
     children: React.ReactNode
 }
 
 export default function Layout ({children}: bodyProps) {
-    return <section className={`${marcellus.className}`}>
+    return <section className={`${firaSans.className}`}>
         <Nav />
         <main>{children}</main>
         <Footer />
